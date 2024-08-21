a9dc1a0cfcb0d21b74a6f67c31bb116bc40a42ee
John Doe <johndoe@email.com>
2024-08-21 14:30:15 -0400

    unabrreviated variable name for readability

--- number syllables.py
+++ number syllables.py
@@ -30,7 +30,7 @@ def count(num):
     if(len(strnum) % 3 !=0):
         while(len(strnum) % 3 !=0):
             strnum= "0"+strnum
-    syl=0 #num of syllables
+    syllables=0 #num of syllables
     end=len(strnum)-1
     if(end>35):
         print("Error: Number too big") #I don't want to deal with varying prefixes past decillion
@@ -38,33 +38,33 @@ def count(num):
     if(num==0): return 2 #return 2 if 0 because ~edge case~
     for i in range(0,len(strnum)):
         if(strnum[i]!="0"): 
-            syl += 1
+            syllables += 1
         else:
             continue
         if(strnum[i]=="7"): #7 is stupid
-            syl += 1
+            syllables += 1
         if(i%3==2 and i!=end and strnum[i-2]=="0" and strnum[i-1]=="0"):
                 #1000,mil,bil,etc.
-                syl+=2
+                syllables+=2
                 if(end-i>14): #quadrillion, quintillion, etc.
-                    syl+=1
+                    syllables+=1
         elif(i%3==1): #check for value in tens place
             if(i>end-3 or strnum[i-1]!="0"):
-                syl+=1 # -ty (last 10's place)
+                syllables+=1 # -ty (last 10's place)
             else:
-                syl +=3 #-ty + thousand, mil etc.
+                syllables +=3 #-ty + thousand, mil etc.
                 if(end-i>14): #quadrillion, quintillion, etc.
-                    syl+=1
+                    syllables+=1
             if(strnum[i]=="1" and strnum[i+1]!="1"): #check if digit is 1 (for 10) and is not 11
-                syl -=1
+                syllables -=1
                 if(strnum[i+1]=="2"): #12
-                    syl-=1 
+                    syllables-=1 
         elif(i%3==0): #100's place
                 if(i>end-3): #last 100's place
-                    syl+=2
+                    syllables+=2
                 else:
-                    syl+=4 #100  + thousand, mil, etc.
+                    syllables+=4 #100  + thousand, mil, etc.
                     if(end-i>14): #quadrillion, quintillion, etc.
-                        syl+=1
-    return syl
+                        syllables+=1
+    return syllables
 main()
