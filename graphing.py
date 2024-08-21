79023dde733443ada60e479e30e917b4548e6159
John Doe <johndoe@email.com>
2024-08-21 16:33:07 -0400

    added graphing.py

---
+++ graphing.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from count import *
+def graph():
+    start=int(input("Start of Range: "))
+    end=int(input("End of Range: "))
+    print("Counting...")
+    x=[]
+    y=[]
+    for i in range(start,end):
+        x.append(i)
+        y.append(count(i))
+    print("Plotting...")
+    plt.plot(x,y, color="green", linewidth=0.5, markersize=2,  marker=".",markerfacecolor='black',markeredgecolor='darkgreen')
+    plt.ticklabel_format(style='plain') 
+    plt.yticks(range(max(y)+2))
+    plt.ylabel("Syllables")
+    plt.title("Syllable Graph")
+    plt.show()
--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 from count import *
-import matplotlib.pyplot as plt
-from numpy.polynomial import Polynomial
+from graphing import *
 def main():
     mode=input("Would you like to create a graph of number syllables?(y/n) ").lower()
     if( mode=="y"):
-        start=int(input("Start of Range: "))
-        end=int(input("End of Range: "))
-        print("Counting...")
-        x=[]
-        y=[]
-        for i in range(start,end):
-            x.append(i)
-            y.append(count(i))
-        print("Plotting...")
-        plt.plot(x,y, color="green", linewidth=0.5, markersize=2,  marker=".",markerfacecolor='black',markeredgecolor='darkgreen')
-        plt.ticklabel_format(style='plain') 
-        plt.yticks(range(max(y)+2))
-        plt.ylabel("Syllables")
-        plt.title("Syllable Graph")
-        plt.show()
+        graph()
+        return
     elif(mode=="n"):
         num=int(input("Number to get Syllables for: "))
         print(count(num))
+        return
     else:
         print("Not a valid response.")
         main()
